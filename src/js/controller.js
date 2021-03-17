30d3c7623ff47666e6ee088a04edef4b0459a3f2
99toxic <nsimpson1983@gmail.com>
2021-03-17 17:59:09 -0400

    new features being added

--- src/js/controller.js
+++ src/js/controller.js
@@ -122,6 +122,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 }; //end controlAddRecipe async fn
 
+const newFeature = function () {
+  console.log('Welcome to the new features!');
+}; //end newFeature fn
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -130,5 +134,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 }; //end init fn
 init();
